e77799fe814f5bee1e650cb6925167a0b1fd4ed6
Ayaz Ahmad <aahmad@codeaurora.org>
2012-05-02 22:39:27 +0530

    FM-APP: Disable FM first when music pause intent is received
    Sound is heard over phone speaker when wired headset
    is plugged out while FM recording and scanning is in
    progress
    
    Disable FM radio immediately when music pause
    intent is received
    
    Change-Id: Ibc06e1f85c1d9f17ecb1ce76da087dd02260cd2e
    CRs-Fixed: 336821

--- src/com/quicinc/fmradio/FMRadioService.java
+++ src/com/quicinc/fmradio/FMRadioService.java
@@ -523,6 +523,9 @@ public class FMRadioService extends Service
                                 return;
                             }
                             if(isFmOn()){
+                                if((mReceiver != null) && mReceiver.disable()) {
+                                   mReceiver = null;
+                                }
                                 fmOff();
                                 if (isOrderedBroadcast()) {
                                     abortBroadcast();
